3db2005f600de25356d46a59d38d72f7804d17e1
zjean <zjean@users.noreply.github.com>
2023-07-05 07:59:48 +0000

    Use custom libdns-transip module to fix fqdn trailing dot.

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/caddyserver/caddy/v2 v2.6.4
-	github.com/libdns/transip v0.0.0-20211011153753-1c0ec90e7a12
+	// github.com/libdns/transip v0.0.0-20211011153753-1c0ec90e7a12
+	github.com/zjean/libdns-transip v0.1.2
 )
--- transip.go
+++ transip.go
@@ -3,7 +3,7 @@ package transip
 import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	"github.com/libdns/transip"
+	"github.com/zjean/libdns-transip"
 )
 
 // Provider wraps the provider implementation as a Caddy module.
